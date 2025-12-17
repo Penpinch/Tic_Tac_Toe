a80d27358201227670b1e16ec23241bc8180746c
Penpinch <brandontorresr747@gmail.com>
2025-12-17 17:56:57 -0600

    restructure the Coordinates class

--- tikTakToe.py
+++ tikTakToe.py
@@ -2,54 +2,78 @@ class Grid: # Initialize the 3x3 structure.
     def __init__(self):
         self.grid = [[0 for i in range(3)] for j in range(3)]
 
-class Coordinates:
-    def __init__(self, x, y):
-        self.setCoordinates(x, y)
+    def isFreeCell(self, coordinates: tuple): 
+        x, y = coordinates
+        return (True if self.grid[x][y] == 0 else False)
 
-    def setCoordinates(self, x, y):
+    def chosenCell(self, coordinates: tuple, mark):
+        x, y = coordinates
+        self.grid[x][y] = mark
+
+    def showCell(self):
+        for fila in range(3):
+            for columna in range(3):
+                print(self.grid[fila][columna], end="\t")
+                if columna >= 2:
+                    print(" ")
+
+class InputCoordinates:
+    @staticmethod
+    def getCoordinates():
+        x = int(input("X: ")) - 1
+        y = int(input("Y: ")) - 1
+        return  x, y
+
+class Coordinates: # setter para el control del ValueError
+    def __init__(self, value: tuple):
+        self.x = 0
+        self.y = 0
+        self.coordinates = value #Automatically calls the setter.
+
+    @property
+    def coordinates(self):
+        return self.x, self.y
+
+    @coordinates.setter
+    def coordinates(self, value):
+        x, y = value
         if not (0 <= x < 3 and 0 <= y < 3):
             raise ValueError("Invalid coordinates. Out of range.")
         self.x = x
         self.y = y
-        return [self.x, self.y]
-
-class PlayerInput(Grid):
-    def __init__(self, position):
-        super().__init__()
-        self.player_x = position[0]
-        self.player_y = position[1]
-
-    def chosenCell(self):
-        self.grid[self.player_x][self.player_y] = 1
-        return self.grid
 
 class Player: 
-    def __init__(self, name, mark_type):
+    def __init__(self, name, mark):
         self.name = name
-        self.figure_type = mark_type
+        self.mark = mark
 
 class Game:
     def __init__(self):
         self.grid = Grid()
 
-    def getCoordinates(self):
-        print("Where to set the token: ")
+    def playerTurn(self, player: Player): # player: Player para que detecte el .name y .mark
+        print(f"{player.name} turn.")
         while True:
             try:
-                x = int(input("X: ")) - 1
-                y = int(input("Y: ")) - 1
-                position = Coordinates(x, y)
-                return position
+                coord = InputCoordinates()
+                coord = coord.getCoordinates()
+                coords = Coordinates(coord)
+                coords = coords.coordinates
+                
+                if self.grid.isFreeCell(coords) == True:
+                    self.grid.chosenCell(coords, player.mark)
+                    break
+                else:
+                    print("Already occuped.")
             except ValueError:
-                print("Invalid coordinates.")
+                print("Invalid coordinates. ")
+        self.grid.showCell()
 
-position = Game().getCoordinates()
-position = [position.x, position.y]
+# testing
+player_1 = Player("Armando", 1)
+player_2 = Player("Juan", 2)
 
-jugada = PlayerInput(position)
-jugada = jugada.chosenCell()
-for fila in range(len(jugada)):
-    for columna in range(len(jugada)):
-        print(jugada[fila][columna], end="\t")
-        if columna >= 2:
-            print(" ")
+game = Game()
+
+game.playerTurn(player_1)
+game.playerTurn(player_2)
