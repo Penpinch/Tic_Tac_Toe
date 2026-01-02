81a4ac772747f3895f5db7a31d4bdbdb389e172e
Penpinch <brandontorresr747@gmail.com>
2026-01-02 14:29:29 -0600

    move Game class logic to setMark method

--- tikTakToe.py
+++ tikTakToe.py
@@ -27,31 +27,6 @@ class Grid: # Initialize the 3x3 structure.
                 if columna >= 2:
                     print(" ")
 
-class InputCoordinates:
-    @staticmethod
-    def getCoordinates():
-        x = int(input("X(1-3): ")) - 1
-        y = int(input("Y(1-3): ")) - 1
-        return  x, y
-
-class Coordinates: # setter para el control del ValueError
-    def __init__(self, value: tuple):
-        self.x = 0
-        self.y = 0
-        self.coordinates = value #Automatically calls the setter.
-
-    @property
-    def coordinates(self):
-        return self.x, self.y
-
-    @coordinates.setter
-    def coordinates(self, value):
-        x, y = value
-        if not (0 <= x < 3 and 0 <= y < 3):
-            raise ValueError("Invalid coordinates. Out of range.")
-        self.x = x
-        self.y = y
-
 class Player: 
     def __init__(self, name, mark):
         self.name = name
@@ -99,63 +74,11 @@ class Winner():
             if self.grid.grid[e][len(self.grid.grid) - 1 - e] == self.player.mark:
                 diagonal_2 += 1
         if diagonal_1 == 3:
-            return self.player.name, self.player.mark
+            return 1, self.player.name, self.player.mark
         if diagonal_2 == 3:
             return 1, self.player.name, self.player.mark
 
         return 0, self.player.name, self.player.mark
-
-class Game:
-    def __init__(self):
-        self.grid = Grid()
-
-    def playerTurn(self, player: Player): # player: Player para que detecte el .name y .mark
-        print(f"{player.name} turn. ({player.mark})")
-        winner_instance = Winner(self.grid, player)
-        draw_result = winner_instance.draw()
-        if draw_result == 2:
-            print("Draw!")
-            return False
-        while True:
-            try:
-                coord = InputCoordinates()
-                coord = coord.getCoordinates()
-                coords = Coordinates(coord)
-                coords = coords.coordinates
-
-                if self.grid.isFreeCell(coords) == True:
-                    self.grid.chosenCell(coords, player.mark)
-                    break
-                else:
-                    print("Already occuped.")
-            except ValueError:
-                print("Invalid coordinates. ")
-
-        self.grid.showCell()
-
-        winner_result, player.name, player.mark = winner_instance.verifyWinner()
-        if winner_result == 1:
-            print(f"The winner is {player.name} ({player.mark})")
-            return False
-        return True
-
-# testing
-"""player_1 = Player("Armando", 1)
-player_2 = Player("Juan", 2)
-game = Game()
-turn = 0"""
-
-"""def Prueba(game: Game, turn, player_1, player_2):
-    game_can_continue = True
-
-    while game_can_continue == True:
-        if turn % 2 == 0:
-            game_can_continue = game.playerTurn(player_1)
-        else:
-            game_can_continue = game.playerTurn(player_2)
-        turn += 1
-
-Prueba(game, turn, player_1, player_2)"""
 
 class Line3D:
     def __init__(self, parent):
@@ -190,8 +113,8 @@ class MyApp(ShowBase):
         self.camera.setPos(0, 0, -10)
         self.camera.lookAt(0, 0, 0)
 
-        """ X:  -3.6  →  +3.6
-            Y:  -2.7  →  +2.7"""
+        #   X:  -3.6  →  +3.6
+        #   Y:  -2.7  →  +2.7
         lines = Line3D(self.render)
 
         lines.addLine((-1.5, 0), (1.5, 0), (0, 0, 0, 1), z = 0) # x1
@@ -200,9 +123,11 @@ class MyApp(ShowBase):
         lines.addLine((0.65, -0.85), (0.65, 2.15), (0, 0, 0, 1), z = 0) # y2
         self.line_np = lines.build()
 
-        self.game = Game()
+        self.grid = Grid()
+        self.turn = 0
         self.player1 = Player("Luis", 1)
         self.player2 = Player("Mario", 2)
+        self.current_player = self.player1
 
         text = CreateText(self.aspect2d)
         self.name_one = text.create_text(self.player1.name, (-0.50, 0.75), 0.07)
@@ -210,111 +135,87 @@ class MyApp(ShowBase):
 
         self.button1 = DirectButton( # top left corner
             scale = (1.521, 0.01, 1.521), pos = (-0.405, 0, 0.163), frameColor = (0, 0, 0, 0), 
-            command = lambda: self.deactivateButton(self.button1))
+            command = lambda: self.setMark(self.button1))
 
-        self.button2 = DirectButton( # bottom left corner
-            scale = (1.521, 0.01, 1.521), pos = (-0.405, 0, -0.65), frameColor = (0, 0, 0, 0), 
-            command = lambda: self.deactivateButton(self.button2))
+        self.button2 = DirectButton( #  top center
+            scale = (2.3, 0.01, 1.521), pos = (0, 0, 0.163), frameColor = (0, 0, 0, 0), 
+            command = lambda: self.setMark(self.button2))
 
         self.button3 = DirectButton( # top right corner
             scale = (1.521, 0.01, 1.521), pos = (0.405, 0, 0.163), frameColor = (0, 0, 0, 0), 
-            command = lambda: self.deactivateButton(self.button3))
+            command = lambda: self.setMark(self.button3))
 
-        self.button4 = DirectButton( # bottom right corner
-            scale = (1.521, 0.01, 1.521), pos = (0.405, 0, -0.65), frameColor = (0, 0, 0, 0), 
-            command = lambda: self.deactivateButton(self.button4))
-
-        self.button5 = DirectButton( #  top corner
-            scale = (2.3, 0.01, 1.521), pos = (0, 0, 0.163), frameColor = (0, 0, 0, 0), 
-            command = lambda: self.deactivateButton(self.button5))
-        
-        self.button6 = DirectButton( #  bottom corner
-            scale = (2.3, 0.01, 1.521), pos = (0, 0, -0.65), frameColor = (0, 0, 0, 0), 
-            command = lambda: self.deactivateButton(self.button6))
-        
-        self.button7 = DirectButton( # left corner
+        self.button4 = DirectButton( # left center
             scale = (1.521, 0.01, 2.3), pos = (-0.405, 0, -0.245), frameColor = (0, 0, 0, 0), 
-            command = lambda: self.deactivateButton(self.button7))
-        
-        self.button8 = DirectButton( # right corner
-            scale = (1.521, 0.01, 2.3), pos = (0.405, 0, -0.245), frameColor = (0, 0, 0, 0), 
-            command = lambda: self.deactivateButton(self.button8))
-        
-        self.button9 = DirectButton( # right corner
+            command = lambda: self.setMark(self.button4))
+
+        self.button5 = DirectButton( # center
             scale = (2.3, 0.01, 2.3), pos = (0, 0, -0.245), frameColor = (0, 0, 0, 0), 
-            command = lambda: self.deactivateButton(self.button9))
-        
-        """self.button1 = DirectCheckButton( # top left corner
-            scale = (0.3, 0.01, 0.26), pos = (-0.28, 0, 0.17), 
-            command = lambda: self.deactivateButton(self.button1))
+            command = lambda: self.setMark(self.button5))
 
-        self.button2 = DirectCheckButton( # bottom left corner
-            scale = (0.3, 0.01, 0.26), pos = (-0.28, 0, -0.66), 
-            command = lambda: self.deactivateButton(self.button2))
+        self.button6 = DirectButton( # right center
+            scale = (1.521, 0.01, 2.3), pos = (0.405, 0, -0.245), frameColor = (0, 0, 0, 0), 
+            command = lambda: self.setMark(self.button6))
 
-        self.button3 = DirectCheckButton( # top right corner
-            scale = (0.3, 0.01, 0.26), pos = (0.54, 0, 0.17), 
-            command = lambda: self.deactivateButton(self.button3))
+        self.button7 = DirectButton( # bottom left corner
+            scale = (1.521, 0.01, 1.521), pos = (-0.405, 0, -0.65), frameColor = (0, 0, 0, 0), 
+            command = lambda: self.setMark(self.button7))
 
-        self.button4 = DirectCheckButton( # bottom right corner 
-            scale = (0.3, 0.01, 0.26), pos = (0.54, 0, -0.66), 
-            command = lambda: self.deactivateButton(self.button4))
+        self.button8 = DirectButton( #  bottom center
+            scale = (2.3, 0.01, 1.521), pos = (0, 0, -0.65), frameColor = (0, 0, 0, 0), 
+            command = lambda: self.setMark(self.button8))
 
-        self.button5 = DirectCheckButton( # top center 
-            scale = (0.44, 0.01, 0.26), pos = (0.19, 0, 0.17), 
-            command = lambda: self.deactivateButton(self.button5))
+        self.button9 = DirectButton( # bottom right corner
+            scale = (1.521, 0.01, 1.521), pos = (0.405, 0, -0.65), frameColor = (0, 0, 0, 0), 
+            command = lambda: self.setMark(self.button9))
 
-        self.button6 = DirectCheckButton( # bottom center
-            scale = (0.44, 0.01, 0.26), pos = (0.19, 0, -0.66), 
-            command = lambda: self.deactivateButton(self.button6))
+        self.button_to_coordinates = {
+            self.button1 : (0, 0), self.button2 : (0, 1), self.button3 : (0, 2), 
+            self.button4 : (1, 0), self.button5 : (1, 1), self.button6 : (1, 2), 
+            self.button7 : (2, 0), self.button8 : (2, 1), self.button9 : (2, 2),
+        }
 
-        self.button7 = DirectCheckButton( # left center 
-            scale = (0.3, 0.01, 0.38), pos = (-0.28, 0, -0.24), 
-            command = lambda: self.deactivateButton(self.button7))
+    def setMark(self, button):
+        try:
+            btn_coords = self.button_to_coordinates[button]
 
-        self.button8 = DirectCheckButton( # right center
-            scale = (0.3, 0.01, 0.38), pos = (0.54, 0, -0.24), 
-            command = lambda: self.deactivateButton(self.button8))
+            if self.grid.isFreeCell(btn_coords) == True:
+                self.grid.chosenCell(btn_coords, self.current_player.mark)
+            else: 
+                return
 
-        self.button9 = DirectCheckButton( # center
-            scale = (0.44, 0.01, 0.38), pos = (0.19, 0, -0.24), 
-            command = lambda: self.deactivateButton(self.button9))"""
-
-        self.buttons = [
-            self.button1, self.button2, self.button3, 
-            self.button4, self.button5, self.button6, 
-            self.button7, self.button8, self.button9]
-        
-        """for btn in self.buttons:
-            btn.hide()"""
-
-
-        #self.taskMgr.add(self.gameLoop, "GameLoop")
-
-    def deactivateButton(self, button):
-        for btn in self.buttons:
-            if btn == button:
-                # --- DirectButton text states = (text, text_pressed, text_rollover, text_disabled) ---
-                button["state"] = "disabled"
-                button.show()
-
-                button["text"] = 'A'
-                button["text_scale"] = 0.1
-                button["text_align"] = TextNode.ACenter
-                button["text_pos"] = (0, 0)
-                button["text_fg"] = (1, 0, 0, 1)
-                button["text_shadow"] = (0, 0, 0, 1)
-                button["relief"] = DirectGuiGlobals.FLAT #DGG.RAISED, DGG.SUNKEN
-
-    def gameLoop(self, task):
-        game_can_continue = True
-        turn = 0
-        while game_can_continue == True:
-            if turn % 2 == 0:
-                game_can_continue = self.game.playerTurn(self.player1)
+            button["state"] = "disabled"
+            if self.current_player.mark == 1:
+                button["image"] = "o_sign.png"
             else:
-                game_can_continue = self.game.playerTurn(self.player2)
-            turn += 1
+                button["image"] = "x_sign.jpg"
+            button["image_scale"] = 0.1
+            button.show()
+
+            self.grid.showCell()
+
+            winner_instance = Winner(self.grid, self.current_player)
+            win_result, winner_player, winner_mark = winner_instance.verifyWinner()
+
+            if win_result == 1:
+                print(f"The winner is {winner_player} ({winner_mark})")
+                for btn in [self.button1, self.button2, self.button3, self.button4, self.button5, self.button6, self.button7, self.button8, self.button9]:
+                    btn["state"] = "disabled"
+                    return
+            elif winner_instance.draw() == 2:
+                print("Draw!")
+                for btn in [self.button1, self.button2, self.button3, self.button4, self.button5, self.button6, self.button7, self.button8, self.button9]:
+                    btn["state"] = "disabled"
+                    return
+            else:
+                self.turn += 1
+                if self.turn % 2 == 0:
+                    self.current_player = self.player1
+                else:
+                    self.current_player = self.player2
+                print(f"{self.current_player.name} turn.")
+        except ValueError:
+            print("Invalid coordinates.")
 
 app = MyApp()
 app.run()
